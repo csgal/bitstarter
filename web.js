0bd5acbe31d5b9bbefa8ced5ca6360411181d19a
Patty Kraft <plkraft@pacbell.net>
2013-07-09 08:36:37 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,14 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-/*jjj
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+   var fs = require('fs');
+   response.send(fs.readFileSync('index.html').toString());
+  //response.send('Hello World 2!');
 });
-*/
 
-var fs = require('fs');
-console.log(fs.readFileSync('index.html').toString());
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
